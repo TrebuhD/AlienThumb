230bf2b5adf40b80371145444d68e9d5dab20946
trebuhd <hubert.dworczynski@gmail.com>
2017-03-30 16:04:41 +0200

    display hot submissions on index page

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
+
+// make config available in routes
+app.locals.config = require('./config')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,8 +30,9 @@ app.use(require('node-sass-middleware')({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', index, function() {
+  console.log("Listening on port 3000, boss!")
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const snoowrap = require('snoowrap')
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  getHotPosts('polska', req.app.locals.config).then(
+    function(result) {
+      res.render('index', { title: 'AlienThumb' , subredditName: 'polska', submissions: result})
+    }
+  );
 });
+
+function getHotPosts(subName, CONFIG) {
+  const r = new snoowrap({
+    userAgent: 'AlienThumb:v0.0.1 (by /u/trebuszek)',
+    clientId: CONFIG.reddit.id,
+    clientSecret: CONFIG.reddit.secret,
+    username: 'AlienThumb',
+    password: CONFIG.reddit.password
+  });
+
+  return r.getSubreddit(subName).getHot().map(submission => submission.title);
+}
 
 module.exports = router;
