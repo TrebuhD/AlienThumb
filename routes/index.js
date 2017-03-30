bba9525c4018e4608847c0abda353750c09a2ceb
trebuhd <hubert.dworczynski@gmail.com>
2017-03-30 23:40:12 +0200

    filter by submission score

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,16 @@ var express = require('express');
 var router = express.Router();
 const snoowrap = require('snoowrap')
 
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  getHotPosts('polska', req.app.locals.config).then(
+  getHotPosts(req.app.locals.config).then(
     function(result) {
       res.render('index', { title: 'AlienThumb' , subredditName: 'polska', submissions: result})
     }
   );
 });
 
-function getHotPosts(subName, CONFIG) {
+function getHotPosts(CONFIG) {
   const r = new snoowrap({
     userAgent: 'AlienThumb:v0.0.1 (by /u/trebuszek)',
     clientId: CONFIG.reddit.id,
@@ -22,7 +20,9 @@ function getHotPosts(subName, CONFIG) {
     password: CONFIG.reddit.password
   });
 
-  return r.getSubreddit(subName).getHot();
+  return r.getSubreddit(CONFIG.reddit.subName).getHot().filter(function(obj) {
+    return (obj.score > CONFIG.reddit.scoreThreshold);
+  });
 }
 
 module.exports = router;
