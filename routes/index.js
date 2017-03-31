eab14f6bb0418510cdfb4f8c82f1974988da0cc4
trebuhd <hubert.dworczynski@gmail.com>
2017-03-31 21:38:48 +0200

    use let and const instead of var

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,14 @@
-var express = require('express');
-var router = express.Router();
-var DatabaseUtils = require('../databaseUtils');
-var config = require('../config');
+const express = require('express');
+const router = express.Router();
+const DatabaseUtils = require('../databaseUtils');
+const config = require('../config');
 
-var db = new DatabaseUtils(config);
+let db = new DatabaseUtils(config);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   require('../submissionPollster').getHotPosts(config).then(
     function (result) {
-      let testObj = {testId: "thisIsIt"};
       db.addToCollection(config.mongodb.collectionName, result);
       res.render('index', { title: 'AlienThumb' , subredditName: config.reddit.subName, submissions: result});
     }
